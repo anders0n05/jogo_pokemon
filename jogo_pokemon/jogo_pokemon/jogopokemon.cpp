0f831ac621cf77f165289bc6c3d03b6c9b757bbf
Anderson Silva <anderson.silva@edu.univali.br>
2017-10-26 22:50:47 -0200

    iniciando bibliotecas e planejando o jogo

---
+++ jogo_pokemon/jogo_pokemon/jogopokemon.cpp
@@ -0,0 +1,14 @@
+// jogopokemon.cpp: Define o ponto de entrada para a aplicação de console.
+//
+
+#include "stdafx.h"
+#include<iostream>
+
+
+int main()
+{
+
+	system("pause");
+    return 0;
+}
+
---
+++ jogo_pokemon/jogo_pokemon/operacoes_funcionais.h
@@ -0,0 +1,23 @@
+#ifndef OPERACOES_FUNCIONAIS_H
+#define OPERACOES_FUNCIONAIS_H
+
+
+
+void inicia_tabuleiro() {
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // !OPERACOES_FUNCIONAIS_H
