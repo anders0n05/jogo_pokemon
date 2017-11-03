a4725eb4afc497f7c68d4933015f89fce29ea0e9
Anderson Silva <anderson.silva@edu.univali.br>
2017-11-03 20:42:51 -0200

    alteracoes

--- jogo_pokemon/jogo_pokemon/operacoes_funcionais.h
+++ jogo_pokemon/jogo_pokemon/operacoes_funcionais.h
@@ -2,61 +2,48 @@
 #define OPERACOES_FUNCIONAIS_H
 #define TAM 7
 #include<string>
+#include<stdlib.h>
+#include<time.h>
 
-
-struct Jogo {//strcut pai, contem todos os elementos em comum seja adversario ou jogador
-	char aparencia;
-};
-struct Jogador:Jogo {
-
+struct Jogador {
+	char nome;
 	
 };
 
-struct Adversario1:Jogo {
-	
+struct Adversario {
+	char nome;
+	int x;
+	int y;
 };
 
-struct Adversario2:Jogo {
-	
-};
-
-struct Adversario3:Jogo {
-	
-};
-
-struct Adversario4:Jogo {
-	
-};
 void inicia_tabuleiro(char matriz[TAM][TAM]) {
-	Jogador jogador; Adversario1 adversario1; Adversario2 adversario2; Adversario3 adversario3; Adversario4 adversario4;
-	jogador.aparencia ='P';
-	adversario1.aparencia = 'A';
-	adversario2.aparencia = 'A';
-	adversario3.aparencia = 'A';
-	adversario4.aparencia = 'A';
-
+	srand(time(NULL));
+	Jogador jogador; Adversario adversario1, adversario2, adversario3, adversario4, posicao_coluna, posicao_linha;
+	jogador.nome = 'P';
+	adversario1.nome = 'A';
+	adversario2.nome = 'A';
+	adversario3.nome = 'A';
+	adversario4.nome = 'A';
+	
+	
+	posicao_coluna.x = rand() % 2;
+	posicao_linha.y = rand() % TAM;
+	if (posicao_coluna.x == 3) {// para nao sortear o lugar do jogador logo de cara
+		posicao_coluna.x = rand() % 2;
+	}
 	for (int i = 0; i < TAM; i++) {
 		for (int j = 0; j< TAM; j++) {
-			matriz[i][j] = ' °';
+			matriz[i][j] = '°';
 			if (i == 3 && j == 3) {
-				matriz[i][j] = jogador.aparencia;
+				matriz[i][j] = jogador.nome;
 			}
-			if (i == 0 && j == 0) {
-				matriz[i][j] = adversario1.aparencia;
-			}
-			if (i == 0 && j == TAM-1) {
-				matriz[i][j] = adversario2.aparencia;
-			}
-			if (i == TAM - 1 && j == 0) {
-				
-				matriz[i][j] = adversario3.aparencia;
-			}
-			if (i == TAM-1 && j == TAM-1) {
-				matriz[i][j] = adversario4.aparencia;
-			}
+		
 		}
 
-		
+		matriz[posicao_coluna.x][posicao_linha.y] = adversario1.nome;
+		matriz[posicao_linha.y][posicao_coluna.x] = adversario2.nome;
+		matriz[posicao_coluna.x][posicao_coluna.x] = adversario3.nome;
+		matriz[posicao_linha.y][posicao_linha.y] = adversario4.nome;
 		
 	}
 
