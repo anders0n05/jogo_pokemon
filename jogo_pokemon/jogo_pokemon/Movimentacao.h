1a4a722b845bd90c7c928f0f5899be5f3fe3e539
Anderson Silva <anderson.silva@edu.univali.br>
2017-11-04 21:49:39 -0200

    alteracoes

--- jogo_pokemon/jogo_pokemon/Movimentacao.h
+++ jogo_pokemon/jogo_pokemon/Movimentacao.h
@@ -2,6 +2,35 @@
 #define MOVIMENTACAO_H
 #include"operacoes_funcionais.h"
 #include<string>
+using namespace std;
+
+
+
+bool move_baixo(int posicao_linha, int posicao_coluna, string matriz[TAM][TAM], bool pegada) {
+
+}
+bool move_esquerda(int posicao_linha, int posicao_coluna, string matriz[TAM][TAM], bool pegada) {
+
+}
+bool move_direito(int posicao_linha, int posicao_coluna, string matriz[TAM][TAM], bool pegada) {
+
+}
+bool move_esquerdo(int posicao_linha, int posicao_coluna, string matriz[TAM][TAM], bool pegada) {
+
+}
+bool move_diagonal_superior_direita(int posicao_linha, int posicao_coluna, string matriz[TAM][TAM], bool pegada) {
+
+}
+bool move_diagonal_superior_esquerda(int posicao_linha, int posicao_coluna, string matriz[TAM][TAM], bool pegada) {
+
+}
+
+bool move_diagonal_inferior_esquerda(int posicao_linha, int posicao_coluna, string matriz[TAM][TAM], bool pegada) {
+
+}
+bool move_diagonal_inferior_direita(int posicao_linha, int posicao_coluna, string matriz[TAM][TAM], bool pegada) {
+
+}
 
 
 
--- jogo_pokemon/jogo_pokemon/operacoes_funcionais.h
+++ jogo_pokemon/jogo_pokemon/operacoes_funcionais.h
@@ -1,14 +1,15 @@
 ﻿#ifndef OPERACOES_FUNCIONAIS_H
 #define OPERACOES_FUNCIONAIS_H
 #define TAM 7
+#define ADVERSARIO 1
 #include<string>
 #include<stdlib.h>
 #include<time.h>
-
+#include"Movimentacao.h"
 using namespace std;
 struct Jogador {
 	string nome;
-	
+
 };
 
 struct Adversario {
@@ -16,9 +17,9 @@ struct Adversario {
 	int x;
 	int y;
 };
-
+bool move_cima(int posicao_linha, int posicao_coluna, string matriz[TAM][TAM]);
 void inicia_tabuleiro(string matriz[TAM][TAM]) {
-	srand(time(NULL));
+	srand(unsigned(time(NULL)));
 	Jogador jogador; Adversario adversario, posicao_coluna, posicao_linha;
 	jogador.nome = "J";
 	adversario.nome[0] = "A1";
@@ -26,13 +27,18 @@ void inicia_tabuleiro(string matriz[TAM][TAM]) {
 	adversario.nome[2] = "A3";
 	adversario.nome[3] = "A4";
 
-	
-	posicao_coluna.x = rand() % 2;
-	posicao_linha.y = rand() % TAM;
-	if (posicao_coluna.x == 3|| posicao_linha.y==3) {// para nao sortear o lugar do jogador logo de cara
-		posicao_coluna.x = 0;
-		posicao_linha.y= 2;
+	for (int i = 0; i < 4; i++) {
+		posicao_coluna.x = rand() % (TAM-1);
+		posicao_linha.y = i + 3;
+		if (posicao_coluna.x == 3) {// para nao sortear o lugar do jogador logo de cara
+			posicao_coluna.x++;
+			
+		}
 	}
+
+	
+
+	
 	for (int i = 0; i < TAM; i++) {
 		for (int j = 0; j< TAM; j++) {
 			matriz[i][j] = " ";
@@ -42,32 +48,42 @@ void inicia_tabuleiro(string matriz[TAM][TAM]) {
 		
 		}
 
-		matriz[posicao_coluna.x][posicao_linha.y] = adversario.nome[0];
-		matriz[posicao_linha.y][posicao_coluna.x] = adversario.nome[1];
-		matriz[posicao_coluna.x][posicao_coluna.x] = adversario.nome[2];
+		matriz[posicao_coluna.x][posicao_coluna.x] = adversario.nome[0];
+		matriz[posicao_coluna.x][posicao_linha.y] = adversario.nome[1];
+		matriz[posicao_linha.y][posicao_coluna.x] = adversario.nome[2];
 		matriz[posicao_linha.y][posicao_linha.y] = adversario.nome[3];
+		
+
+		move_cima(posicao_linha, posicao_coluna, matriz);
 		
 	}
 
 }
 
 
-
-bool busca_personagem(int posicao_linha, int posicao_coluna, string matriz[TAM][TAM], bool pegada) {
+bool move_cima(int posicao_linha, int posicao_coluna, string matriz[TAM][TAM]) {
 	Adversario adversario; Jogador jogador;
 	inicia_tabuleiro(matriz);
 	if (matriz[posicao_linha][posicao_coluna] == jogador.nome) {
 		return true;
 	}
-	else if (matriz[posicao_linha + 1][posicao_coluna] == " ") {
+	else if (posicao_coluna <0 || posicao_linha < 0 || posicao_linha>TAM || posicao_coluna>TAM) {
+		return false;
+	}
+	else if (matriz[posicao_linha - 1][posicao_coluna] != " ") {
+		return false;
+	}
+	else {
 
-
-		matriz[posicao_linha + 1][posicao_coluna] = adversario.nome[0];
+		matriz[posicao_linha - 1][posicao_coluna] = adversario.nome[0];
+		if (move_cima(posicao_linha - 1, posicao_coluna, matriz) {
+			return true;
+		}
 	}
 
-
-
 }
+
+
 
 
 
